deac2e77c5a572e9fe3e79e68009e8e13ec9f8ce
ZedWAreHK <zedware.myself@gmail.com>
2024-12-07 04:34:24 +0800

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,7 +1,7 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.dev/getting-started/configuration/
-export default R2Explorer({ readonly: true, basicAuth: [{
+export default R2Explorer({ readonly: false, basicAuth: [{
     username: 'zedware',
     password: 'zdw123456'
   },{
